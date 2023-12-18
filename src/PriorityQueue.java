156ca32332832fbd0ec48bebd6fb4094160a4711
Marie <mariev718@lpsk12.org>
2023-12-17 18:40:13 -0700

    y

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+
 /**
  * This class represents a Priority Queue (heap) based on the ordering
  * defined by the compareTo method for the element. "Lower" value will
@@ -10,7 +11,10 @@ import java.util.ArrayList;
  */
 public class PriorityQueue<E extends Comparable<E>> {
 
+
     private ArrayList<E> myHeap; //array representation of the heap
+    private int size = 0;
+
 
     /**
      * Creates an empty Priority Queue
@@ -19,6 +23,7 @@ public class PriorityQueue<E extends Comparable<E>> {
         myHeap = new ArrayList<E>();
     }
 
+
     /**
      * Adds the element to the priority queue
      *
@@ -26,26 +31,51 @@ public class PriorityQueue<E extends Comparable<E>> {
      */
     public void add(E element) {
 
-        int index = myHeap.size();
-        int depth = (int) (Math.log(index) / Math.log(2));
 
-        myHeap.set(index, element);
+        if (size == 0) {
 
-        for(int i = index; i > 0; i -= (2^depth)) {
 
-            int temp = i - 2^depth;
+            myHeap.set(0, element);
 
-            if(temp < 0) temp = 0;
-            if(myHeap.get(index) < myHeap.get(temp)) {
 
-                swap(index, temp);
-                depth--;
+        } else {
+
+
+            size++;
+
+
+            int index = size;
+
+
+            myHeap.set(index, element);
+
+
+            for (int i = index; i > 0; i -= (i / 2 + 1)) {
+
+
+                int temp = i - (i / 2 + 1);
+
+
+                if (temp < 0) temp = 0;
+
+
+                if (myHeap.get(i).compareTo(myHeap.get(temp)) < 0) {
+
+
+                    swap(i, temp);
+
+
+                }
+
 
             }
 
+
         }
 
+
     }
+
 
     /**
      * Swaps two elements in the queue.
@@ -56,12 +86,15 @@ public class PriorityQueue<E extends Comparable<E>> {
      */
     private void swap(int posOne, int posTwo) {
 
+
         E temp1 = myHeap.get(posOne);
         E temp2 = myHeap.get(posTwo);
         myHeap.set(posOne, temp2);
         myHeap.set(posTwo, temp1);
 
+
     }
+
 
     /**
      * Returns whether or not the element is in the heap
@@ -70,8 +103,28 @@ public class PriorityQueue<E extends Comparable<E>> {
      * @return true if the element is in the queue, false otherwise
      */
     public boolean contains(E element) {
+
+
+        for (int i = 0; i < myHeap.size(); i++) {
+
+
+            if (myHeap.get(i).equals(element)) {
+
+
+                return true;
+
+
+            }
+
+
+        }
+
+
         return false;
+
+
     }
+
 
     /**
      * Returns the element of highest priority, null if queue is empty.
@@ -80,8 +133,14 @@ public class PriorityQueue<E extends Comparable<E>> {
      * @return the element of highest priority queue
      */
     public E peek() {
+
+
+        if (myHeap.size() != 0) return myHeap.get(0);
         return null;
+
+
     }
+
 
     /**
      * Removes and returns the element of highest priority,
@@ -90,8 +149,45 @@ public class PriorityQueue<E extends Comparable<E>> {
      * @return the element of highest priority
      */
     public E poll() {
-        return null;
+
+
+        if (size == 0) {
+
+
+            return null;
+
+
+        }
+
+
+        E temp = myHeap.get(0);
+
+
+        myHeap.set(0, null);
+
+
+        if (size == 1) {
+
+
+            size--;
+            return temp;
+
+
+        }
+
+
+        swap(0, size);
+        size--;
+
+
+        heapify(0);
+
+
+        return temp;
+
+
     }
+
 
     /**
      * Will "sift down" the element at the given position
@@ -101,7 +197,88 @@ public class PriorityQueue<E extends Comparable<E>> {
      */
     private void heapify(int pos) {
 
+
+        int index = pos;
+
+
+        if (size == index) return;
+
+
+        while (size > (index * 2 + 1)) {
+
+
+            E parent = myHeap.get(index);
+            E child1 = myHeap.get(index * 2 + 1);
+
+
+            if (size > (index * 2 + 2)) {
+
+
+                E child2 = myHeap.get(index * 2 + 2);
+
+
+                if (parent.compareTo(child1) > 0) {
+
+
+                    if (child1.compareTo(child2) < 0) {
+
+
+                        swap(index, index * 2 + 1);
+
+
+                        index = index * 2 + 1;
+
+
+                    }
+
+
+                } else if (parent.compareTo(child2) > 0) {
+
+
+                    swap(index, index * 2 + 2);
+
+
+                    index = index * 2 + 2;
+
+
+                } else {
+
+
+                    index = size;
+
+
+                }
+
+
+            } else {
+
+
+                if (parent.compareTo(child1) > 0) {
+
+
+                    swap(index, index * 2 + 1);
+
+
+                    index = index * 2 + 1;
+
+
+                } else {
+
+
+                    index = size;
+
+
+                }
+
+
+            }
+
+
+        }
+
+
     }
+
 
     /**
      * Finds and removes the given element from the queue.
@@ -112,8 +289,55 @@ public class PriorityQueue<E extends Comparable<E>> {
      * @return true if an element was removed from the queue, false otherwise
      */
     public boolean remove(E element) {
+
+
+        if (size == 0) return false;
+
+
+        if (size == 1) {
+
+
+            if (myHeap.get(0).equals(element)) {
+
+
+                myHeap.set(0, null);
+                size--;
+                return true;
+
+
+            }
+
+
+            return false;
+
+
+        }
+
+
+        for (int i = 0; i < size; i++) {
+
+
+            if (myHeap.get(i).equals(element)) {
+
+
+                myHeap.set(i, null);
+                swap(i, size - 1);
+                size--;
+                heapify(i);
+                return true;
+
+
+            }
+
+
+        }
+
+
         return false;
+
+
     }
+
 
     /**
      * Returns the number of elements in the queue
@@ -121,8 +345,13 @@ public class PriorityQueue<E extends Comparable<E>> {
      * @return the number of elements in the queue
      */
     public int size() {
-        return -1;
+
+
+        return size;
+
+
     }
+
 
     /**
      * Returns the String representation of the heap
@@ -132,7 +361,23 @@ public class PriorityQueue<E extends Comparable<E>> {
      * @return the String representation of the heap
      */
     public String toString() {
-        return "toString";
+
+
+        String temp = "";
+
+
+        for (int i = 0; i < size; i++) {
+
+
+            temp += myHeap.get(i);
+            temp += " ";
+
+
+        }
+
+
+        return temp;
+
     }
 
 
@@ -143,8 +388,8 @@ public class PriorityQueue<E extends Comparable<E>> {
      * @param args
      */
     public static void main(String[] args) {
-// TODO Auto-generated method stub
 
     }
+
 
 }
